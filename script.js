0eeaa048af27c7a3912503ca171ffbcdf2864363
GregoireGeoffroy <thegathering92@gmail.com>
2024-07-26 15:23:58 +0300

    setup hover effect

--- script.js
+++ script.js
@@ -3,5 +3,11 @@ const container = document.querySelector("#container");
 for (let i = 0; i < 256; i++) {  // 16x16 = 256 squares
   const square = document.createElement("div");
   square.classList.add("square");
+
+  // Add event listener for mouseover to change background color
+  square.addEventListener('mouseover', () => {
+    square.style.backgroundColor = 'lightblue'; // Change to any color you like
+  });
+
   container.appendChild(square);
 }
